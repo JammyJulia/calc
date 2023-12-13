4a85929926e7d9a582ac846563ac566f8289a9ad
Julija Osipova <josipova6@edu.riga.lv>
2023-12-13 12:15:33 +0200

    code :D

--- calc.py
+++ calc.py
@@ -1,24 +1,33 @@
 
 # konvertē datu mērvienības
 def megabyte_to_megabit(megabyte):
-   result = 0
+   result = megabyte * 8
    return result
 
 # saskaiti degvielas patēriņu uz 100 km
 def fuel_consumption(litres, kilometers):
-   result = 0
+   result = litres / (kilometers / 100)
    return result
 
 # konvertē temperatūru
 def celsius_to_fahrenheit(celsius):
-    result = 0
+    result = celsius * 1.8 + 32 
     return result
 
 # saliec visus skaitļus pirms dota skaitļa (izmanto for)
 def sigma(tail):
     result = 0
+    for i in range(tail+1):
+        result += i
     return result
 
 # nokonvertē svaru uz tuvāko mērvienību - grams, kilograms, tonna (izmanto if)
 def weight(grams):
-    return str(grams) + "g"
+    if grams >= 1000 and grams < 100000:
+        return str(grams//1000) + "kg"
+    elif grams >= 100000 and  grams < 1000000:
+        return str(grams//100000) + "c"
+    elif grams >= 1000000:
+        return str(grams//1000000) + "t"
+    else:
+        return str(grams) + "g"    
